f855a13203ad4c2264c3aa94e414e82dd2d2d712
MAD-CRAZY-MAN <MAD-CRAZY-MAN>
2019-06-27 19:19:46 +0900

    Modified : main.c

--- atmega128a/project/Electronic_Piano/main.c
+++ atmega128a/project/Electronic_Piano/main.c
@@ -7,19 +7,8 @@
 
 
 #include <avr/io.h>
-#define F_CPU 16000000
-#include <util/delay.h>
+#include "lcd.h"
 
-#define FUNCSET 0x28 //function set
-#define ENTMODE 0x06 //entry mode set
-#define ALLCLR 0x01 //all clear
-#define DISPON 0x0c //display on
-
-void LCD_INIT(void);
-void COMMAND(unsigned char byte);
-void DATA(unsigned char byte);
-void STRING(unsigned char font[], unsigned char n);
-void MOVE(int y, int x);
 void DISPLAY_SHIFT(char p);
 void IO_SETTING(void);
 void INTRODUCE(int num);
@@ -28,13 +17,12 @@ void MENU(void);
 void PIANO_RECORD(void);
 void LISTEN(void);
 
-int flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0, flag5 =0;
-int maxTime = -1, maxWhat= -1;
 
-unsigned int recordTime[100] = {};
-unsigned int recordWhat[100] = {};
-int timeCnt;
-int whatCnt;
+int flag1 = 0, flag2 = 0, flag3 = 0, flag4 = 0, flag5 =0;
+int maxCnt = 0;
+
+unsigned int recordTime[200] = {};
+unsigned int recordWhat[200] = {};
 
 int main(void)
 {
@@ -45,8 +33,7 @@ int main(void)
 
 	while (1)
 	{
-		
-		
+
 		PIANO_PLAY();
 		if(!(PINC&0x20))				
 		{
@@ -54,8 +41,7 @@ int main(void)
 			_delay_ms(50);
 			MENU();
 			INTRODUCE(1);
-		}
-		
+		}		
 	}
 }
 
@@ -90,6 +76,7 @@ void MENU(void)
 						{
 							while(!(PINC&0x40));
 							LISTEN();
+							while(!(PINC&0x40));
 							INTRODUCE(2);
 						}
 						break;
@@ -115,6 +102,137 @@ void MENU(void)
 	}
 }
 
+void PIANO_RECORD(void)
+{
+	unsigned char message[] = "Recording...";
+	unsigned char message2[] = "COMPLETE!";
+	int mode = 1;
+	PORTE = 0x01;
+	COMMAND(ALLCLR);
+	
+	MOVE(1, 4);
+	STRING(message, 12);
+
+	int _time = 0;
+	int Cnt = 0;
+	while(mode)
+	{
+		if(!(PINC&0x40)) // record complete
+		{
+			mode = 0;
+			COMMAND(ALLCLR);
+			MOVE(1, 5);
+			STRING(message2, 9);
+			_delay_ms(1000);
+			
+			while(!(PINC&0x40));
+			_delay_ms(30);
+		}
+		else if(!(PINC&0x01)) //check do time
+		{
+			recordWhat[Cnt++] = 1;
+			
+			while(!(PINC&0x01))
+			{
+				_delay_ms(50);
+				_time++;
+				MOVE(2,5);
+				DATA(0x00);
+			}
+			MOVE(2, 5);
+			DATA(' ');
+			
+			recordTime[Cnt] = _time;
+			_time = 0;
+		}
+		else if(!(PINC&0x02)) //check re time
+		{
+			recordWhat[Cnt++] = 2;
+			
+			while(!(PINC&0x02))
+			{
+				_delay_ms(50);
+				_time++;
+				MOVE(2,7);
+				DATA(0x00);
+			}
+			MOVE(2, 7);
+			DATA(' ');
+			
+			recordTime[Cnt] = _time;
+			_time = 0;
+		}
+		else if(!(PINC&0x04)) //check do time
+		{
+			recordWhat[Cnt++] = 3;
+			
+			while(!(PINC&0x04))
+			{
+				_delay_ms(50);
+				_time++;
+				MOVE(2,9);
+				DATA(0x00);
+			}
+			MOVE(2, 9);
+			DATA(' ');
+			
+			recordTime[Cnt] = _time;
+			_time = 0;
+		}
+		else if(!(PINC&0x08)) //check do time
+		{
+			recordWhat[Cnt++] = 4;
+			
+			while(!(PINC&0x08))
+			{
+				_delay_ms(50);
+				_time++;
+				MOVE(2, 11);
+				DATA(0x00);
+			}
+			MOVE(2, 11);
+			DATA(' ');
+			
+			recordTime[Cnt] = _time;
+			_time = 0;
+		}
+		else if(!(PINC&0x10)) //check do time
+		{
+			recordWhat[Cnt++] = 5;
+			
+			while(!(PINC&0x10))
+			{
+				_delay_ms(50);
+				_time++;
+				MOVE(2,13);
+				DATA(0x00);
+			}
+			MOVE(2, 13);
+			DATA(' ');
+			
+			recordTime[Cnt] = _time;
+			_time = 0;
+		}
+		else if(PINC == 0b01111111)
+		{
+			recordWhat[Cnt++] = 0;
+			
+			while(PINC == 0b01111111)
+			{
+				_delay_ms(50);
+				_time++;
+			}
+
+			recordTime[Cnt] = _time;
+			_time = 0;
+		}
+	}
+	maxCnt = Cnt;
+	maxCnt = Cnt;
+	PORTE = 0x00;
+	INTRODUCE(2);
+}
+
 void LISTEN(void)
 {
 	unsigned char message[] = "Listening...";
@@ -122,16 +240,20 @@ void LISTEN(void)
 	unsigned char empty[] = "Record Empty!!";
 	
 	int mode = 1;
-	timeCnt = -1, whatCnt = -1;
+	int iCnt = 0;
 	int delay = 0, i;
+	
+	COMMAND(ALLCLR);
+	MOVE(1, 3);
+	STRING(message, 12);
 	while(mode)
 	{
-		if(whatCnt >= maxWhat) //if listend to the end, return
+		if(iCnt == maxCnt) //if listend to the end, return
 		{
-			if(maxWhat == -1)
+			if(maxCnt == 0)
 			{
 				COMMAND(ALLCLR);
-				MOVE(2, 1);
+				MOVE(1, 1);
 				STRING(empty, 14);
 				_delay_ms(1000);
 				break;
@@ -154,208 +276,110 @@ void LISTEN(void)
 				break;
 			}
 		}
-			
-		switch (recordWhat[++whatCnt]) {
-			case 0 : COMMAND(ALLCLR);
-					 MOVE(1, 4);
-					 STRING(message, 12);
-					 delay = recordTime[++timeCnt];
+
+		switch (recordWhat[iCnt++]) {
+			case 0 : delay = recordTime[iCnt];
 					 for(i = 0; i<delay;i++)
+					 {
+						if(!(PINC&0x40)) //if press okay, return;
+						{
+							mode = 0;
+							break;
+						}
 						_delay_ms(50);
+					 }
 					 break;
 					 
-			case 1 : COMMAND(ALLCLR);
-					 MOVE(1, 4);
-					 STRING(message, 12);
-					 MOVE(2, 5);
+			case 1 : MOVE(2, 5);
 					 DATA(0x00);
 					 
-					 delay = recordTime[++timeCnt];
+					 delay = recordTime[iCnt];
 					 for(i=0;i<delay;i++)
+					 {
+						if(!(PINC&0x40)) //if press okay, return;
+				 		{
+					 		mode = 0;
+					 		break;
+				 		}
 						_delay_ms(50);
+					 }
+					 MOVE(2, 5);
+					 DATA(' ');
 					 break;
 					 
-			case 2 : COMMAND(ALLCLR);
-					MOVE(1, 4);
-					STRING(message, 12);
+			case 2 :MOVE(2, 7);
+					DATA(0x00);
+					
+					delay = recordTime[iCnt];
+					for(i=0;i<delay;i++)
+					{
+						_delay_ms(50);
+						if(!(PINC&0x40)) //if press okay, return;
+						{
+							mode = 0;
+							break;
+						}
+					}
 					MOVE(2, 7);
-					DATA(0x00);
-					
-					delay = recordTime[++timeCnt];
-					for(i=0;i<delay;i++)
-						_delay_ms(50);
+					DATA(' ');
 					break;
 					
-			case 3 : COMMAND(ALLCLR);
-					MOVE(1, 4);
-					STRING(message, 12);
+			case 3 :MOVE(2, 9);
+					DATA(0x00);
+					
+					delay = recordTime[iCnt];
+					for(i=0;i<delay;i++)
+					{
+						if(!(PINC&0x40)) //if press okay, return;
+						{
+							mode = 0;
+							break;
+						}
+						_delay_ms(50);
+					}
 					MOVE(2, 9);
-					DATA(0x00);
-					
-					delay = recordTime[++timeCnt];
-					for(i=0;i<delay;i++)
-						_delay_ms(50);
+					DATA(' ');
 					break;
 					
-			case 4 : COMMAND(ALLCLR);
-					MOVE(1, 4);
-					STRING(message, 12);
-					MOVE(2, 11);
+			case 4 :MOVE(2, 11);
 					DATA(0x00);
 					
-					delay = recordTime[++timeCnt];
+					delay = recordTime[iCnt];
 					for(i=0;i<delay;i++)
+					{
+						if(!(PINC&0x40)) //if press okay, return;
+						{
+							mode = 0;
+							break;
+						}
 						_delay_ms(50);	
+					}
+					MOVE(2, 11);
+					DATA(' ');
 					break;
-			case 5 : COMMAND(ALLCLR);
-					MOVE(1, 4);
-					STRING(message, 12);
-					MOVE(2, 13);
+					
+			case 5 :MOVE(2, 13);
 					DATA(0x00);
 					
-					delay = recordTime[++timeCnt];
+					delay = recordTime[iCnt];
 					for(i=0;i<delay;i++)
-					_delay_ms(50);
-						break;
+					{
+						if(!(PINC&0x40)) //if press okay, return;
+						{
+							mode = 0;
+							break;
+						}
+					   _delay_ms(50);
+					}
+					MOVE(2, 13);
+					DATA(' ');
+					break;
 		}
-	
-		if(!(PINC&0x40)) //if press okay, return;
-		{
-			mode = 0;
-			while(!(PINC&0x40));
-			_delay_ms(30);
-		}
-	}
-}
-void PIANO_RECORD(void)
-{
-	unsigned char message[] = "Recording...";
-	unsigned char message2[] = "COMPLETE!";
-	int mode = 1;
-	COMMAND(ALLCLR);
-	
-	MOVE(1, 4);
-	STRING(message, 12);
-
-	int _time = 0;
-	timeCnt = -1;
-	whatCnt = -1;		
-	while(mode)
-	{	
-		if(!(PINC&0x40)) // record complete
-		{
-			mode = 0;
-			COMMAND(ALLCLR);
-			MOVE(1, 5);
-			STRING(message2, 9);
-			_delay_ms(1000);
-			
-			while(!(PINC&0x40));
-			_delay_ms(30);
-		}
-		else if(!(PINC&0x01)) //check do time
-		{
-			recordWhat[++whatCnt] = 1;
 		
-			while(!(PINC&0x01))
-			{
-				_delay_ms(50);
-				_time++;
-				MOVE(2,5);
-				DATA(0x00);
-			}
-			MOVE(2, 5);
-			DATA(' ');
-			
-			recordTime[++timeCnt] = _time;
-			_time = 0;
-		}
-		else if(!(PINC&0x02)) //check re time
-		{
-			recordWhat[++whatCnt] = 2;
-			
-			while(!(PINC&0x02))
-			{
-				_delay_ms(50);
-				_time++;
-				MOVE(2,7);
-				DATA(0x00);
-			}
-			MOVE(2, 7);
-			DATA(' ');
-			
-			recordTime[++timeCnt] = _time;
-			_time = 0;
-		}
-		else if(!(PINC&0x04)) //check do time
-		{
-			recordWhat[++whatCnt] = 3;
-			
-			while(!(PINC&0x04))
-			{
-				_delay_ms(50);
-				_time++;
-				MOVE(2,9);
-				DATA(0x00);
-			}
-			MOVE(2, 9);
-			DATA(' ');
-			
-			recordTime[++timeCnt] = _time;
-			_time = 0;
-		}
-		else if(!(PINC&0x08)) //check do time
-		{
-			recordWhat[++whatCnt] = 4;
-			
-			while(!(PINC&0x08))
-			{
-				_delay_ms(50);
-				_time++;
-				MOVE(2, 11);
-				DATA(0x00);
-			}
-			MOVE(2, 11);
-			DATA(' ');
-			
-			recordTime[++timeCnt] = _time;
-			_time = 0;
-		}
-		else if(!(PINC&0x10)) //check do time
-		{
-			recordWhat[++whatCnt] = 5;
-			
-			while(!(PINC&0x10))
-			{
-				_delay_ms(50);
-				_time++;
-				MOVE(2,13);
-				DATA(0x00);
-			}
-			MOVE(2, 13);
-			DATA(' ');
-			
-			recordTime[++timeCnt] = _time;
-			_time = 0;
-		}
-		else
-		{
-			recordWhat[++whatCnt] = 0;
-			
-			while(PINC == 0b01111111)
-			{
-				_delay_ms(50);
-				_time++;
-			}
-
-			recordTime[++timeCnt] = _time;
-			_time = 0;
-		}
 	}
-	maxTime = timeCnt;
-	maxWhat = whatCnt;
-	INTRODUCE(2);
 }
+
+
 void INTRODUCE(int num)
 {
 		/* num == 1, print piano start
@@ -376,7 +400,7 @@ void INTRODUCE(int num)
 			MOVE(1, 7);
 			STRING(menu, 4);
 		}
-		else //start, hello
+		else if (num == 0)//start, hello
 		{
 			MOVE(1,4);
 			STRING(start, 11);
@@ -478,7 +502,8 @@ void IO_SETTING(void)
 {
 	DDRD = 0xFF; //LCD lines output
 	DDRC = 0x00; //SW lines input
-	
+	DDRE = 0xFF; //LED lines output
+	PORTC = 0x00;
 	int iCnt;
 	unsigned int pName[] =
 	{
@@ -489,126 +514,7 @@ void IO_SETTING(void)
 	for(iCnt=0;iCnt<8;iCnt++)
 		DATA(pName[iCnt]);
 }
-void DISPLAY_SHIFT(char p)
-{
-	if ( p == 'R')
-	{
-		COMMAND(0x1c);
-	}
-	if (p == 'L')
-	{
-		COMMAND(0x18);
-	}
-}
-void COMMAND(unsigned char byte)
-{
-	_delay_ms(2);
 
-	PORTD = byte & 0xF0;	//명령어 쓰기
-	PORTD &= 0b11111100;	//RS = 0, RW = 0
-	_delay_us(1);			//RS & RW setup
-	
-	PORTD |= 0b00000100;	//E = 1, lcd동작
-	_delay_us(1);			//E pulse width time
-	PORTD &= 0b11111011;	//E = 0
-	
-	PORTD = (byte<<4) & 0xF0; //명령어 쓰기
-	PORTD &= 0b11111100;
-	_delay_us(1);
-	
-	PORTD |= 0b00000100;  //E = 1, lcd동작
-	_delay_us(1);
-	PORTD &= 0b11111011;  //E = 0
-}
-
-void MOVE(int y, int x)
-{
-	unsigned char data = 0;
-	if(y==1)
-	{
-		data = 0x80 + x - 1;
-	}
-	else if(y == 2)
-	{
-		data = 0xC0 + x - 1;
-	}
-	
-	COMMAND(data);
-}
-void STRING(unsigned char font[], unsigned char n)
-{
-	_delay_ms(2);
-	int i = 0;
-	for(;n>0;n--)
-	{
-		PORTD = font[i] & 0xf0;//상위 4비트 명령어 쓰기
-		PORTD |= 0b00000001;//RS = 1, 데이터 설정
-		PORTD &= 0b11111101;//RW = 0, 쓰기 설정
-		_delay_us(1);
-		PORTD |= 0b00000100;//E = 1, lcd 동작
-		_delay_us(1);
-		PORTD &= 0b11111011;//E = 0
-		
-		PORTD = (font[i]<<4) & 0xF0;	 //하위 4비트 데이터 쓰기
-		PORTD |= 0b00000001;		//RS = 1, 데이터 설정
-		PORTD &= 0b11111101;		//RW  = 0, 쓰기 설정
-		_delay_us(1);
-		PORTD |= 0b00000100;//E = 1, lcd 동작
-		_delay_us(1);
-		PORTD &= 0b11111011;//E = 0;
-		
-		_delay_us(40);
-		i++;
-	}
-}
-void DATA(unsigned char byte)
-{
-	_delay_ms(2);
-	
-	PORTD = byte & 0xf0;//상위 4비트 명령어 쓰기
-	PORTD |= 0b00000001;//RS = 1, 데이터 설정
-	PORTD &= 0b11111101;//RW = 0, 쓰기 설정
-	_delay_us(1);
-	PORTD |= 0b00000100;//E = 1, lcd 동작
-	_delay_us(1);
-	PORTD &= 0b11111011;//E = 0
-	
-	PORTD = (byte<<4) & 0xF0;	 //하위 4비트 데이터 쓰기
-	PORTD |= 0b00000001;		//RS = 1, 데이터 설정
-	PORTD &= 0b11111101;		//RW  = 0, 쓰기 설정
-	_delay_us(1);
-	PORTD |= 0b00000100;//E = 1, lcd 동작
-	_delay_us(1);
-	PORTD &= 0b11111011;//E = 0;
-}
-
-
-
-void LCD_INIT(void)
-{
-	_delay_ms(30);		//전원 투입 후 30ms 이상 지연
-	
-	//Function set
-	//인터페이스(DL) = 0(4bit), 라인(N) = 1(2라인), 폰트(F) = 0(5*8 dot)
-	COMMAND(FUNCSET);
-	_delay_us(40);
-
-	//Display ON/OFF Control
-	//화면 표시(D) = 1(on), 커서(C) = 0(off), 블링크(B) = 0(off)
-	COMMAND(DISPON);
-	_delay_us(40);
-
-	//Clear Display
-	//화면 클리어, 커서 홈위치
-	COMMAND(ALLCLR);
-	_delay_ms(1.53);
-	
-	//Entry Mode Set
-	//커서 방향(I/D) = 1(address증가), 표시이동(S)=0(이동하지 않음
-	//COMMAND(ENTMODE);
-	_delay_us(40);
-	
-}
 
 
 
