81be7f3033bf1ac92437635b5928eef0f2dcbe56
MAD-CRAZY-MAN <awake_noh@naver.com>
2019-10-05 20:13:44 +0900

    Add: kill parent process

--- Linux/Signal/Exercise/kill.c
+++ Linux/Signal/Exercise/kill.c
@@ -6,28 +6,42 @@
 #include <stdio.h>
 #include <signal.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+
+void quit();
 
 void main(void)
 {
-	pid_t pid;
+	pid_t pid, status, parentId;
 	int count = 5;
+	
+	pid = fork();
 
-	if((pid = fork()) > 0) //부모 프로세스
+	if(pid == 0) //자식 프로세스
 	{
-		sleep(2);
-		kill(pid, SIGINT);
-		raise(SIGINT);
-		printf("[parent] bye!\n");
-	}
-	else if(pid == 0) //자식 프로세스
-	{
-		while(count)
+		parentId = getppid();
+		while (count)
 		{
 			printf("[childe] count is %d \n", count--);
 			sleep(1);
 		}
+		kill(parentId, SIGALRM);
+		printf("[childe] bye!\n");
+
+		raise(SIGKILL);
+	}
+	else if(pid>0) //부모 프로세스
+	{
+		signal(SIGALRM, quit);
+		wait(&status);//pause()를 이용해도 됨
 	}
 	else	
 		printf("fail to fork\n");
 }
-		
+
+void quit()
+{
+	printf("[parent] bye!\n");
+	raise(SIGKILL);
+	//kill(getpid(), SIGKILL);
+}
